33cfdd34b34bd1e285f83a1796f02e4f885a2319
Heo Jong Tae <heo9910@naver.com>
2020-11-30 19:03:43 +0900

    .

---
+++ src/main/java/com/study/boot/init/TestDataInitializer.java
@@ -0,0 +1,45 @@
+package com.study.boot.init;
+
+import java.util.stream.IntStream;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.study.boot.domain.Board;
+import com.study.boot.repository.BoardRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Profile("dev")
+@Component
+public class TestDataInitializer {
+    
+    @Autowired
+    private TestDataSetter testDataSetter;
+    
+    @PostConstruct
+    void init() {
+        testDataSetter.insertData();
+    }
+
+}
+
+@Profile("dev")
+@Component
+@RequiredArgsConstructor
+class TestDataSetter {
+    
+    private final BoardRepository boardRepository;
+    
+    @Transactional
+    void insertData() {
+        IntStream.range(0, 200).forEach(n -> {
+            Board board = new Board("제목 : " + n, "user0" + (n % 10), "게시물 내용 ... " + n + " 내용 채우기");
+            boardRepository.save(board);
+        });
+    }
+}
--- src/test/java/com/study/boot/repository/BoardRepositoryTest.java
+++ src/test/java/com/study/boot/repository/BoardRepositoryTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -43,7 +44,7 @@ class BoardRepositoryTest {
         }
         
         Board findBoard = resultOpt.get();
-        
+
         assertThat(findBoard).isNotNull();
         assertThat(findBoard.getBno()).isEqualTo(board.getBno());
         assertThat(findBoard.getTitle()).isEqualTo(board.getTitle());
