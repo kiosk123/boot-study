015f2cc727c6ffcfa332c28053f2e852f30cb7a7
Heo Jong Tae <heo9910@naver.com>
2020-11-30 19:04:08 +0900

    .

--- src/test/java/com/study/boot/repository/BoardRepositoryTest.java
+++ src/test/java/com/study/boot/repository/BoardRepositoryTest.java
@@ -7,13 +7,10 @@ import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceUnit;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
