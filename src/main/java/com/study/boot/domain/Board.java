95bca6d4e30971556706502df9c136c385628cd6
Heo Jong Tae <heo9910@naver.com>
2020-12-22 01:17:21 +0900

    .

--- src/main/java/com/study/boot/domain/Board.java
+++ src/main/java/com/study/boot/domain/Board.java
@@ -1,9 +1,15 @@
 package com.study.boot.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jdo.annotations.Column;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.study.boot.domain.base.BaseTimeEntity;
@@ -31,6 +37,9 @@ public class Board extends BaseTimeEntity {
     
     @Setter
     private String content;
+    
+    @OneToMany(mappedBy = "board", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private List<BoardReply> replies = new ArrayList<>();
 
     public Board(String title, String writer, String content) {
         this.title = title;
