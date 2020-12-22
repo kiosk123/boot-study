c43e5fdbaeefc1e7e567bfce935b38c6159fe27c
Heo Jong Tae <heo9910@naver.com>
2020-12-22 23:51:43 +0900

    .

--- src/main/java/com/study/boot/dto/BoardDTO.java
+++ src/main/java/com/study/boot/dto/BoardDTO.java
@@ -4,13 +4,14 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Setter @Getter
 @ToString
+@NoArgsConstructor
 public class BoardDTO {
     private Long bno;
     private String title;
